3e538390ef7b78b3848df082c5dbd73d38cfcd45
hmigl <88746406+hmigl@users.noreply.github.com>
2023-02-04 14:23:22 -0300

    refactor: delegate tool registration to VuttrService

--- src/main/java/com/api/vuttr/web/VuttrController.java
+++ src/main/java/com/api/vuttr/web/VuttrController.java
@@ -1,5 +1,6 @@
 package com.api.vuttr.web;
 
+import com.api.vuttr.domain.VuttrService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,8 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/tools")
 public class VuttrController {
 
+  private final VuttrService service;
+
+  public VuttrController(VuttrService service) {
+    this.service = service;
+  }
+
   @PostMapping
   public ResponseEntity<ToolDTO> registerTool(@RequestBody @Valid ToolDTO tool) {
-    return ResponseEntity.status(HttpStatus.CREATED).body(tool);
+    return ResponseEntity.status(HttpStatus.CREATED).body(service.registerTool(tool));
   }
 }
