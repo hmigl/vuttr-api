6a5161f57f548d0958913387d9bef60e7afcb93a
hmigl <88746406+hmigl@users.noreply.github.com>
2023-02-04 13:46:04 -0300

    wip: create VuttrService

---
+++ src/main/java/com/api/vuttr/domain/VuttrService.java
@@ -0,0 +1,11 @@
+package com.api.vuttr.domain;
+
+import com.api.vuttr.web.ToolDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VuttrService {
+  public ToolDTO registerTool(ToolDTO tool) {
+    return null;
+  }
+}
