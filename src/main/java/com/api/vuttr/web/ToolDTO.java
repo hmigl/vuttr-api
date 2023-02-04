871ddfb8a2f0ca758b6a7edee5151016101356f0
hmigl <88746406+hmigl@users.noreply.github.com>
2023-02-04 13:38:30 -0300

    feat: apply constraints to validate json request

--- src/main/java/com/api/vuttr/web/ToolDTO.java
+++ src/main/java/com/api/vuttr/web/ToolDTO.java
@@ -1,11 +1,16 @@
 package com.api.vuttr.web;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import org.hibernate.validator.constraints.URL;
+import org.hibernate.validator.constraints.UniqueElements;
+
 import java.util.List;
 
 public record ToolDTO(
-    String title,
-    String link,
-    String description,
-    List<String> tags,
+    @NotBlank String title,
+    @URL String link,
+    @NotBlank String description,
+    @NotEmpty @UniqueElements List<String> tags,
     Integer id) {
 }
