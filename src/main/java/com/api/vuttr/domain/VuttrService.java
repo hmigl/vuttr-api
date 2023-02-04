893be945a31c88a784ca98d909fa90165ff5ed15
hmigl <88746406+hmigl@users.noreply.github.com>
2023-02-04 14:18:02 -0300

    feat: create VuttrRepository

--- src/main/java/com/api/vuttr/domain/VuttrService.java
+++ src/main/java/com/api/vuttr/domain/VuttrService.java
@@ -1,11 +1,20 @@
 package com.api.vuttr.domain;
 
+import com.api.vuttr.persistence.Tool;
+import com.api.vuttr.persistence.VuttrRepository;
 import com.api.vuttr.web.ToolDTO;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VuttrService {
+
+  private final VuttrRepository repository;
+
+  public VuttrService(VuttrRepository repository) {
+    this.repository = repository;
+  }
+
   public ToolDTO registerTool(ToolDTO tool) {
-    return null;
+    return ToolDTO.fromEntity(repository.save(Tool.fromDTO(tool)));
   }
 }
