ab366fa3f1e68d40a76263fabaa21a51434a38e3
hmigl <88746406+hmigl@users.noreply.github.com>
2023-02-07 09:33:38 -0300

    feat: create method to retrieve the persisted tools

--- src/main/java/com/api/vuttr/domain/VuttrService.java
+++ src/main/java/com/api/vuttr/domain/VuttrService.java
@@ -3,7 +3,13 @@ package com.api.vuttr.domain;
 import com.api.vuttr.persistence.Tool;
 import com.api.vuttr.persistence.VuttrRepository;
 import com.api.vuttr.web.ToolDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class VuttrService {
@@ -16,5 +22,10 @@ public class VuttrService {
 
   public ToolDTO registerTool(ToolDTO tool) {
     return ToolDTO.fromEntity(repository.save(Tool.fromDTO(tool)));
+  }
+
+  public ResponseEntity<Page<ToolDTO>> retrieveTools(Pageable pageable) {
+    List<ToolDTO> tools = repository.findAll(pageable).stream().map(ToolDTO::fromEntity).toList();
+    return ResponseEntity.ok(new PageImpl<>(tools, pageable, tools.size()));
   }
 }
--- src/main/java/com/api/vuttr/web/VuttrController.java
+++ src/main/java/com/api/vuttr/web/VuttrController.java
@@ -2,12 +2,12 @@ package com.api.vuttr.web;
 
 import com.api.vuttr.domain.VuttrService;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/tools")
@@ -22,5 +22,10 @@ public class VuttrController {
   @PostMapping
   public ResponseEntity<ToolDTO> registerTool(@RequestBody @Valid ToolDTO tool) {
     return ResponseEntity.status(HttpStatus.CREATED).body(service.registerTool(tool));
+  }
+
+  @GetMapping
+  public ResponseEntity<Page<ToolDTO>> retrieveTools(@PageableDefault Pageable pageable) {
+    return service.retrieveTools(pageable);
   }
 }
