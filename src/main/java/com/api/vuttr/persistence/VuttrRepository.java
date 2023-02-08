57613ef89b7834c86829e36701f9c2b1b19d44df
hmigl <88746406+hmigl@users.noreply.github.com>
2023-02-08 12:46:10 -0300

    feat: define query to retrieve tools by tag

--- src/main/java/com/api/vuttr/persistence/VuttrRepository.java
+++ src/main/java/com/api/vuttr/persistence/VuttrRepository.java
@@ -1,7 +1,14 @@
 package com.api.vuttr.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface VuttrRepository extends JpaRepository<Tool, Integer> {}
+public interface VuttrRepository extends JpaRepository<Tool, Integer> {
+  @Query("SELECT t FROM Tool t JOIN t.tags tg WHERE tg = :tag")
+  List<Tool> findAllByTag(@Param("tag") String tag);
+}
